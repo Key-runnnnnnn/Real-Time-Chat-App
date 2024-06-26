21e279a91d48b478ebb4dd0c96fd4c20b7fd4538
kiran <kiran.s.dhawan@gmail.com>
2024-06-26 15:19:00 +0530

    Added user routes

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -16,7 +16,7 @@ const protectRoute = async (req, res, next) => {
     if (!user) {
       return res.status(404).json({ error: 'User not found' });
     }
-    req.User = user;
+    req.user = user;
     next();
   } catch (error) {
     console.log('Error in protectRoute middleware:', error.message);
--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,9 @@ import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 
 import authRoutes from './routes/auth.routes.js'
-import connectToMongoDB from './db/connectToMongoDB.js'
 import messegeRoutes from './routes/messege.routes.js'
+import userRoutes from './routes/user.routes.js'
+import connectToMongoDB from './db/connectToMongoDB.js'
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -16,6 +17,7 @@ app.use(express.json()) // to parse the incoming request with JSON payloads(req.
 app.use(cookieParser()) // to parse the incoming request cookies
 app.use("/api/auth",authRoutes);
 app.use("/api/messeges",messegeRoutes);
+app.use("/api/users",userRoutes);
 
 
 app.listen(PORT, () => {
